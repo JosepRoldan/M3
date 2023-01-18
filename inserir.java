50a1af2b5e21436b8a3be0cae19e65d01cef93a4
JosepRoldan <94633274+JosepRoldan@users.noreply.github.com>
2023-01-18 13:04:21 +0100

    Update inserir.java

--- inserir.java
+++ inserir.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class arrays {
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String[] noms = new String[10];
+        for (int i = 0; i < noms.length; i++){
+            String nom = scanner.next();
+            noms[i] = nom;
+        }
+        int x = scanner.nextInt();
+        System.out.println(noms[x]);
+    }
+}
